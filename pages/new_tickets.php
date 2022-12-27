2baa15f5960faa71914198eda71083b239bf9800
Kasun Dulara <kasundularaam@gmail.com>
2022-12-28 01:33:50 +0530

    working on new tickets

--- pages/new_tickets.php
+++ pages/new_tickets.php
@@ -1,5 +1,47 @@
 <?php
 
+require("../config/database_helper.php");
+
+
+function authorize()
+{
+    session_start();
+    $indexNo = $_SESSION["indexNo"];
+    if ($indexNo != "Admin") {
+        header("Location: index.php");
+    }
+}
+
+function getTicketTypes()
+{
+    $eventId = $_GET["eventId"] ?? "";
+    if (!empty($eventId)) {
+        try {
+            $helper = new DatabaseHelper();
+            $helper->connect();
+            $sql = "SELECT * FROM events WHERE eventId = :eventId";
+            $params = ["eventId" => $eventId];
+            $stmt = $helper->query($sql, $params);
+            $event = $stmt->fetch();
+            $helper->close();
+            $types = array();
+            if (!empty($event["generalAdmission"])) {
+                array_push($types, "generalAdmission");
+            }
+            if (!empty($event["vipPass"])) {
+                array_push($types, "vipPass");
+            }
+            return $types;
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+}
+
+authorize();
+
+$ticketTypes = getTicketTypes();
+
 ?>
 
 <!DOCTYPE html>
@@ -8,7 +50,16 @@
 <?php $page = "New Tickets";
 include("../components/header.php"); ?>
 
-<h1>Add Tickets</h1>
+<form class="auth-form" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+    <?php foreach ($ticketTypes as $ticketType) : ?>
+        <div class="input-field">
+            <label for="indexNo">Index No</label>
+            <input type="text" name="indexNo" id="indexNo" value="<?php echo $indexNo ?>">
+            <div class="input-error"><?php echo $errors["indexNo"] ?>
+            </div>
+        </div>
+    <?php endforeach; ?>
+</form>
 
 <?php include "../components/footer.php" ?>
 
