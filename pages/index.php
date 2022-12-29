007e8feec28b0bd8bc4604c6212591bbaeb0bc0c
Kasun Dulara <kasundularaam@gmail.com>
2022-12-30 03:20:18 +0530

    index page restyled

--- pages/index.php
+++ pages/index.php
@@ -99,81 +99,65 @@ include("../components/header.php"); ?>
 
 <div class="welcome-view">
   <img src="https://images.unsplash.com/photo-1501281668745-f7f57925c3b4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" alt="">
-  <div class="trance-container">
-  </div>
-
 </div>
 
 <div class="container">
+  <br>
   <h2>FEATURED EVENTS</h2>
-
-
   <br>
   <div class="events-grid">
 
     <?php foreach ($events as $event) : ?>
-      <div class="event-card">
-        <img src="<?php echo $event["imageUrl"] ?>" alt="">
-        <div class="details">
+      <div class="card">
+        <img class="width100 card-image" src="<?php echo $event["imageUrl"] ?>" alt="">
+        <div class="container">
+          <br>
           <h2> <a href="event.php?eventId=<?php echo $event['eventId'] ?>"> <?php echo $event["title"] ?></a></h2>
           <br>
-          <div class="info">
-            <div class="icon-text">
-              <img class="icon" src="../images/calendar.png" alt="">
+          <div class="row space-between">
+            <div class="row gap10 dark f-large">
+              <img class="icon-small" src="../images/calendar.png" alt="">
               <?php echo date("d.m.Y", strtotime($event["date"])) . " at " . date('g:ia', strtotime($event["time"])); ?>
             </div>
-            <div class="icon-text">
-              <img class="icon" src="../images/place.png" alt="">
+            <div class="row gap10 dark f-large">
+              <img class="icon-small" src="../images/place.png" alt="">
               <?php echo $event["place"] ?>
             </div>
           </div>
           <hr>
-          <p><?php echo $event["description"] ?></p>
-          <br>
-          <div class="entrance-details">
-            <h4>Entrance</h4>
-
+          <div class="card-description dark f-large">
+            <?php echo $event["description"] ?>
+          </div>
+          <hr>
+          <div class="row space-around">
             <?php if (isFree($event)) : ?>
-              <div class="free entrance-methods">
-                <div class="icon-text">
-                  <img class="icon" src="../images/free.png" alt="">
-
-                  Free
-                </div>
+              <div class="row align-center gap10 primary bold f-large">
+                <img class="icon-small" src="../images/free.png" alt="">
+                Free
               </div>
             <?php endif; ?>
 
             <?php if (isGeneralAdmission($event)) : ?>
-              <div class="generalAdmission entrance-methods">
-                <div class="icon-text">
-                  <img class="icon" src="../images/ticket.png" alt="">
-
-                  <?php echo "Rs: " . getTicket($event, "generalAdmission")["price"]; ?>
-                </div>
+              <div class="row gap10 red bold">
+                <img class="icon-small" src="../images/ticket.png" alt="">
+                <?php echo "Rs: " . getTicket($event, "generalAdmission")["price"]; ?>
               </div>
             <?php endif; ?>
 
             <?php if (isVipPass($event)) : ?>
-              <div class="vipPass entrance-methods">
-                <div class="icon-text">
-                  <img class="icon" src="../images/vip.png" alt="">
-                  <?php echo "VIP Rs: " . getTicket($event, "vipPass")["price"]; ?>
-                </div>
+              <div class="row gap10 gold bold">
+                <img class="icon-small" src="../images/vip.png" alt="">
+                <?php echo "VIP Rs: " . getTicket($event, "vipPass")["price"]; ?>
               </div>
             <?php endif; ?>
-
           </div>
+          <br>
         </div>
-
       </div>
     <?php endforeach; ?>
-
   </div>
 </div>
-
-
-
-
+<br>
 <?php include "../components/footer.php" ?>
 
 </html>
