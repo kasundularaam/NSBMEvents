d927cbafdd1eae369aa1649030c45bea675e1f99
Kasun Dulara <kasundularaam@gmail.com>
2022-12-29 15:00:03 +0530

    qr error fixed

--- pages/profile.php
+++ pages/profile.php
@@ -34,11 +34,41 @@ function getUser()
 
 
 
+function getBookings($userId)
+{
+    try {
+        $helper = new DatabaseHelper();
+
+        $helper->connect();
+
+        $sql = "SELECT * FROM bookings WHERE userId = :userId";
+        $params = ["userId" => $userId];
+        $stmt = $helper->query($sql, $params);
+        $bookings = $stmt->fetchAll();
+        $helper->close();
+
+        return $bookings;
+    } catch (\Throwable $th) {
+        throw $th;
+    }
+}
+
+function getQrCode($bookingId)
+{
+    global $type;
+    $response = file_get_contents("https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=HELLO");
+    $base64 = 'data:image/' . $type . ';base64,' . base64_encode($response);
+    return $base64;
+}
+
+
 if (isset($_POST["sign-out"])) {
     signOut();
 }
 
 $user = getUser();
+
+$bookings = getBookings($user["indexNo"]);
 
 ?>
 
@@ -47,6 +77,19 @@ $user = getUser();
 
 <?php $page = "Profile";
 include("../components/header.php"); ?>
+
+<script src="../html2canvas.js"></script>
+<script>
+    function capture() {
+        html2canvas(document.getElementById("3"), {
+            letterRendering: 1,
+            allowTaint: true,
+        }).then(function(canvas) {
+            console.log(canvas.toDataURL("image/png", 0.9));
+        })
+
+    }
+</script>
 
 
 <form class="user-details-card card" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
@@ -66,6 +109,24 @@ include("../components/header.php"); ?>
 
     <input class="auth-btn" name="sign-out" type="submit" value="Sign Out">
 </form>
+
+<?php foreach ($bookings as $booking) : ?>
+    <div class="card" id="<?php echo $booking["bookingId"] ?>">
+        <?php echo $booking["bookingId"] ?>
+        <br>
+        <img src="<?php echo getQrCode($booking["bookingId"]) ?>" alt="QR">
+        <!-- <img src="../images/admin.png" alt="QR"> -->
+        <br>
+        <?php echo $booking["ticketId"] ?>
+        <br>
+        <?php echo $booking["timestamp"] ?>
+        <br>
+    </div>
+    <button id="btn" onclick="capture();" class="btn">Download</button>
+    <hr>
+
+
+<?php endforeach; ?>
 
 <?php include "../components/footer.php" ?>
 
